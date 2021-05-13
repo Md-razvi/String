958807debeab8bf258b22496faee42f97b0e4c02
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-13 23:17:19 +0530

    Add files via upload

---
+++ String_pr4.java
@@ -0,0 +1,10 @@
+// Extract a string using Substring
+
+public class String_pr4 {
+    public static void main(String[] args) 
+    {
+            String g="Harry_is_all_right";
+            String sub=g.substring(9,12);
+            System.out.println(sub);
+    }
+}
