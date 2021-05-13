988ac71ba2695e42d968cc3d4944443fdc4fdc37
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-13 22:38:44 +0530

    Add files via upload

---
+++ String_pr1.java
@@ -0,0 +1,14 @@
+// Different ways  creating a string
+public class String_pr1
+{
+    public static void main(String[] args) {
+        // Method1 Java String literals
+        String s1="Sapphire is blue";
+        //Or by new keyword
+        String s=new String("S is for Sapphire");
+        System.out.println(s1);
+        System.out.println(s);
+
+
+    }
+}
