7f7b50e1e8ad2b0fd18588b9860ebe41d9093da7
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-13 23:17:38 +0530

    Add files via upload

---
+++ String_pr3.java
@@ -0,0 +1,8 @@
+//Calculating the length of String 
+public class String_pr3 {
+    public static void main(String[] args) {
+        String m="Helloworld";
+        // Simply "String".length() should be used 
+        System.out.println(m.length());
+    }
+}
