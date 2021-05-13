1d28bdacfe80043424a4454fd949f43bb15b734f
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-14 01:44:56 +0530

    Add files via upload

---
+++ String_pr5.java
@@ -0,0 +1,8 @@
+//Searching in strings using indexOf()
+public class String_pr5 {
+   public static void main(String[] args) {
+       String s="Hellooooo NewYork";
+       // Searches first occurences of New York
+       System.out.println(s.indexOf("NewYork"));
+   } 
+}
