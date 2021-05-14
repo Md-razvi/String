4636d1bca198db58292eb14d62e60e1b6d06ea1d
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-14 07:05:52 +0530

    Add files via upload

---
+++ String_pr13.java
@@ -0,0 +1,12 @@
+public class String_pr13 {
+    public static void main(String[] args) {
+        int a=2354;
+        String f=" Yes this is String Alliright";
+        System.out.println(a);
+        String b;
+        b=String.valueOf(a);
+        System.out.println(b+f); 
+
+     }
+
+}
---
+++ String_pr14.java
@@ -0,0 +1,13 @@
+public class String_pr14
+
+{
+    public static void main(String[] args) {
+        String U="String of LowerCase to UpperCase ";
+        String L="STRING OF UPPERCASE TO LOWERCASE";
+        String s=U.toUpperCase(),g=L.toLowerCase();
+        System.out.println(s);
+        System.out.println(g);
+
+    }
+    
+}
