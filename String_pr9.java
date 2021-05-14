839b8dfeebb84c2b83f500d56cb16d76fdb712c2
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-14 06:25:12 +0530

    Add files via upload

---
+++ String_pr9.java
@@ -0,0 +1,9 @@
+public class String_pr9 {
+    public static void main(String[] args){
+    String s="Hello  My  Name is Blah ";
+    System.err.print(s.trim());//  trim removes the trailing and leading spaces
+    s="What is";
+    System.err.print(" "+s.trim());
+    
+    }
+}
