d0ee1a2d981f8b2b86f989c610e3d5d8d9bdae77
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-14 06:24:45 +0530

    Add files via upload

---
+++ String_pr10.java
@@ -0,0 +1,8 @@
+public class String_pr10 {
+    public static void main(String[] args) {
+        //replace method replaces the certain charcter in string
+        String s="Hello World";
+        String new1=s.replace('l', 'z');
+        System.out.println( new1);
+    }
+}
